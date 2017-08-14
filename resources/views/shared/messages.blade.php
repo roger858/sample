ae795fc9d1c67bdb6868a2265cf9417bb20f691e
roger858 <roger858@126.com>
2017-08-14 01:28:54 +0000

    Add password resets & email configuration

--- resources/views/shared/messages.blade.php
+++ resources/views/shared/messages.blade.php
@@ -1,8 +1,8 @@
-@foreach (['danger', 'warning', 'success', 'info'] as $msg)
+@foreach (['danger', 'warning', 'success', 'info', 'status'] as $msg)
   @if(session()->has($msg))
     <div class="flash-message">
-      <p class="alert alert-{{$msg}}">
-        {{session()->get($msg)}}
+      <p class="alert alert-{{ $msg }}">
+        {{ session()->get($msg) }}
       </p>
     </div>
   @endif
